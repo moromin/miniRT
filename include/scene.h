2a77b0ce7264767193474d39cbe43ef5d2247b9a
moromin <34294957+moromin@users.noreply.github.com>
2022-05-02 13:38:54 +0900

    refactor: use wrapper function to simplify processes

--- include/scene.h
+++ include/scene.h
@@ -7,10 +7,13 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
+# include "../utils/get_next_line.h"
+# include "../utils/utils.h"
+# include "../wrapper/x.h"
+
 // Error messages
 # define ERR_INVALID_ARGS "Invalid number of arguments"
 # define ERR_INVALID_FILE "File is invalid (expected '*.rt' file)"
-# define ERR_GNL_FAILED "Failed to Get_Next_Line"
 
 void	read_rt_file(int argc, char **argv);
 
