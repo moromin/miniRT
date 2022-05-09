c20f981b1c1a45fe90da972045ddabe34f9999fa
Takumi Hara <takumihara1226@gmail.com>
2022-05-09 20:56:05 +0900

    refac: change raytrace() to object's method

--- src/object.c
+++ src/object.c
@@ -1,34 +1,47 @@
 #include <assert.h>
 
 #include "../include/object.h"
+#include "../include/miniRT.h"
+#include "../include/light.h"
 
-static double	solve_ray_equation_(t_object *me, t_ray ray);
-static int		raytrace_(t_object *me, t_vector cross_point, t_vector light);
+static double	object_solve_ray_equation_(t_object *me, t_ray ray);
+static int		object_raytrace_(
+					t_object *me, t_vector cross_point, t_vector light);
 
 void	object_ctor(t_object *const me, t_vector center)
 {
-	static t_object_vtbl const	vtbl = {
-			&solve_ray_equation_,
-			&raytrace_
+	static t_object_vtbl	vtbl = {
+			&object_solve_ray_equation_,
+			&object_raytrace_
 	};
 
 	me->vptr = &vtbl;
 	me->center = center;
 }
 
-static double	solve_ray_equation_(t_object *const me, t_ray ray)
+static double	object_solve_ray_equation_(t_object *const me, t_ray ray)
 {
 	(void)me;
 	(void)ray;
 	assert(0);
 }
 
-static int	raytrace_(t_object *me, t_vector cross_point, t_vector light)
+/*
+ * radiance_ambient: 環境光の反射光の放射輝度
+ * radiance_specular: 直接光の鏡面反射光の放射輝度
+ * radiance_diffuse: 拡散反射光の放射輝度
+ *
+ */
+// normal: 正規化された物体面の法線ベクトル
+// incident_direction: 正規化された入射方向ベクトル
+static int	object_raytrace_(
+		t_object *me_, t_vector cross_point, t_vector light_point)
 {
-	(void)me;
-	(void)cross_point;
-	(void)light;
-	assert(0);
+	const t_sphere	*me = (t_sphere*)me_;
+	double			radiance;
+
+	radiance = calc_radiance(me->super.center, cross_point, light_point);
+	return (rgb_to_int(radiance, radiance, radiance));
 }
 
 double	object_solve_ray_equation(t_object *const me, t_ray ray)
