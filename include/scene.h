d1cb2c27d1bf1510cd63a2a57ed235042519b62c
moromin <34294957+moromin@users.noreply.github.com>
2022-05-10 12:36:16 +0900

    wip: load rt file test

--- include/scene.h
+++ include/scene.h
@@ -11,10 +11,15 @@
 # include "../utils/utils.h"
 # include "../wrapper/x.h"
 
+// Element type
+# define AMBIENT_LIGHTING 0
+
 // Error messages
 # define ERR_INVALID_ARGS "Invalid number of arguments"
 # define ERR_INVALID_FILE "File is invalid (expected '*.rt' file)"
+# define ERR_INVALID_NUM_OF_ELEMENT_INFO "Invalid number of element information"
 
+// scene.c
 void	read_rt_file(int argc, char **argv);
 
 #endif
--- src/scene.c
+++ src/scene.c
@@ -1,5 +1,43 @@
 #include "../include/scene.h"
 
+static void	load_rt_parameters(int type, char **info)
+{
+	char	**params;
+
+	while (*info)
+	{
+		params = x_split(*info++, ',');
+		// TODO: load parameters to t_program
+		if (type == AMBIENT_LIGHTING)
+		{
+			int i = 0;
+			double val;
+			while (params[i])
+			{
+				ft_strtod(params[i], &val);
+				printf("%lf ", val);
+				i++;
+			}
+		}
+		printf("\n");
+		free_2d_array((void ***)&params);
+	}
+}
+
+static void	load_rt_element(char *line)
+{
+	size_t	num;
+	char	**info;
+
+	info = x_split(line, ' ');
+	num = count_2d_array((void ***)&info);
+	if (num == 3 && !ft_strcmp(info[0], "A"))
+		load_rt_parameters(AMBIENT_LIGHTING, ++info);
+	else
+		printf("%s\n", ERR_INVALID_NUM_OF_ELEMENT_INFO);
+	free_2d_array((void ***)&info);
+}
+
 static void	check_rt_file(char *filename)
 {
 	int		fd;
@@ -12,7 +50,7 @@ static void	check_rt_file(char *filename)
 		status = x_get_next_line(fd, &line);
 		if (status == GNL_STATUS_DONE)
 			break ;
-		printf("%s\n", line);
+		load_rt_element(line);
 	}
 	free(line);
 	x_close(fd);
