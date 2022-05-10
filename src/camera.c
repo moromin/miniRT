1f316430b1d0be33a8b956c4f07c92137db6f8ec
Takumi Hara <takumihara1226@gmail.com>
2022-05-10 15:56:49 +0900

    refac: better func size

--- src/camera.c
+++
@@ -1,6 +0,0 @@
-#include "../include/vector.h"
-
-t_vector	init_camera_point(void)
-{
-	return (vec_init(0, 0, -5));
-}
--- src/main.c
+++ src/main.c
@@ -1,14 +1,28 @@
+#include <printf.h>
+#include <stdlib.h>
 #include "../minilibx-linux/mlx.h"
 #include "../include/vector.h"
 #include "../include/miniRT.h"
 #include "../include/light.h"
 
 #define BACKGROUND 0x6495ED
+#define OBJECT_NUM 1
 
 void	init_program(t_program *program)
 {
 	program->mlx = mlx_init();
 	program->win = mlx_new_window(program->mlx, WIDTH, HEIGHT, "miniRT");
+	init_image(program, &program->img);
+	// camera setup
+	program->camera_point = vec_init(0, 0, -5);
+	// light setup
+	program->light = light(vec_init(-5, 5, -5), color(1.0, 1.0, 1.0));
+	program->ambient = ambient(color(0.1, 0.1, 0.1), color(0.01, 0.01, 0.01));
+	// object setup
+	// todo: malloc check, malloc free
+	program->object = malloc(sizeof(t_object *) * OBJECT_NUM);
+	program->object[0] = (t_object *)malloc(sizeof(t_sphere));
+	sphere_ctor((t_sphere *)program->object[0], 1.0, vec_init(0, 0, 5), color(0.69, 0.0, 0.0), color(0.3, 0.3, 0.3));
 }
 
 // https://knzw.tech/raytracing/?page_id=1243
@@ -26,19 +40,18 @@ void	draw(t_program *program, int x, int y)
 	t_ray		ray;
 	double		ray_coefficient;
 	t_vector	cross_point;
-	int			color;
+	t_color		color;
 
 	screen_point = init_screen_point(x, y);
 	ray.start = program->camera_point;
 	ray.direction = vec_sub(screen_point, program->camera_point);
-	ray_coefficient = object_solve_ray_equation(&program->object, ray);
+	ray_coefficient = object_solve_ray_equation(program->object[0], ray);
 	if (ray_coefficient >= 0)
 	{
 		cross_point = vec_add(ray.start,
 				  vec_mult(ray.direction, ray_coefficient));
-		color = object_raytrace(
-				&program->object, cross_point, program->light_point);
-		add_color_to_image(&program->img, color, x, y);
+		color = object_calc_radiance(program->object[0], cross_point, program->light, program->ambient);
+		add_color_to_image(&program->img, color_to_int(color), x, y);
 	}
 	else
 		add_color_to_image(&program->img, BACKGROUND, x, y);
@@ -70,15 +83,10 @@ int	main(void)
 
 	// mlx setup
 	init_program(&program);
-	init_image(&program, &program.img);
-	// camera setup
-	program.camera_point = init_camera_point();
-	// light setup
-	program.light_point = init_light_point();
-	// object setup
-	sphere_ctor((t_sphere *)&program.object, 1.0, vec_init(0, 0, 5));
 	// create_image
 	create_image(&program);
+	// mlx setup
 	mlx_put_image_to_window(program.mlx, program.win, program.img.image, 0, 0);
 	mlx_loop(program.mlx);
+	return (EXIT_SUCCESS);
 }
