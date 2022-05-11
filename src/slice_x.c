9eabf1eb0d6b2cdeacbadb4344f42574e7231120
Takumi Hara <takumihara1226@gmail.com>
2022-05-11 22:48:33 +0900

    feat: slice util func & encapsulation

---
+++ src/slice_x.c
@@ -0,0 +1,32 @@
+#include "../include/slice.h"
+
+struct s_slice {
+	void	*org_start;
+	void	*cur_start;
+	size_t	size;
+	size_t	len;
+	size_t	cap;
+};
+
+void	delete_recursively(t_slice *s, int depth)
+{
+	size_t	i;
+	t_slice	*next;
+
+	depth--;
+	i = 0;
+	while (i < s->len)
+	{
+		next = *(t_slice **)s->org_start + s->size * i;
+		if (depth == 0)
+			delete(next);
+		else
+			delete_recursively(next, depth);
+	}
+	delete(s);
+}
+
+void	*get_x2(t_slice *s, size_t i1, size_t i2)
+{
+	return (get(*(t_slice **)get(s, i1), i2));
+}
