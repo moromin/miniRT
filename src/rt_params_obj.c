b237cc715dd22a4d328ecb5a63d46397b46e7694
Takumi Hara <takumihara1226@gmail.com>
2022-05-11 23:40:13 +0900

    fix: actually check misconfiguration

--- src/rt_params_obj.c
+++ src/rt_params_obj.c
@@ -12,7 +12,7 @@ char	*load_sphere(t_program *p, char **info)
 		return (ERR_MISCONFIGURED_SPHERE);
 	if (!ft_strtod(info[1], &radius))
 		return (ERR_MISCONFIGURED_SPHERE);
-	if (!get_color_from_str(info[2], &col) && check_color_range(col, 0.0, 255.0))
+	if (!(get_color_from_str(info[2], &col) && check_color_range(col, 0.0, 255.0)))
 		return (ERR_MISCONFIGURED_SPHERE);
 	sphere_ctor(get(sp, 0), radius, center, col, color(0, 0, 0));
 	append(p->objects, &sp);
@@ -29,10 +29,10 @@ char	*load_plane(t_program *p, char **info)
 	pl = make(sizeof(t_plane), 1, 1);
 	if (!get_vector_from_str(info[0], &center))
 		return (ERR_MISCONFIGURED_PLANE);
-	if (!get_vector_from_str(info[1], &normal)
-			&& check_vector_range(normal, -1.0, 1.0))
+	if (!(get_vector_from_str(info[1], &normal)
+			&& check_vector_range(normal, -1.0, 1.0)))
 		return (ERR_MISCONFIGURED_PLANE);
-	if (!get_color_from_str(info[2], &col) && check_color_range(col, 0.0, 255.0))
+	if (!(get_color_from_str(info[2], &col) && check_color_range(col, 0.0, 255.0)))
 		return (ERR_MISCONFIGURED_PLANE);
 	plane_ctor(get(pl, 0), center, normal, col, color(0, 0, 0));
 	append(p->objects, &pl);
