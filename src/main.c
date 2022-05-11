dd1d180a42ac91d95fe71703fc0830d15a52b06b
Takumi Hara <takumihara1226@gmail.com>
2022-05-11 12:09:35 +0900

    feat: shadow

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 #include "../minilibx-linux/mlx.h"
 #include "../include/vector.h"
 #include "../include/miniRT.h"
-#include "../include/light.h"
 #include "../include/scene.h"
 #include "../include/math.h"
 
@@ -51,19 +50,22 @@ t_vector	init_screen_point(int x, int y)
 			0));
 }
 
-int	closest_object(t_object **objects, t_ray ray)
+int	closest_object(t_object **objects, t_ray ray, double max_dist, bool early_return)
 {
 	const int	index = ({
 			int	res = -1;
-			double ray_coefficient;
 			double	min_ray_coefficient = INFINITY;
 
 			for (int i = 0; objects[i]; i++){
-				ray_coefficient = object_solve_ray_equation(objects[i], ray);
+				double ray_coefficient = object_solve_ray_equation(objects[i], ray);
 				if (ray_coefficient < 0)
 					continue;
-				if (chmin(&min_ray_coefficient, ray_coefficient))
+				t_vector cross_point = vec_add(ray.start, vec_mult(ray.direction, ray_coefficient));
+				double dist = vec_magnitude(vec_sub(ray.start, cross_point));
+				if (chmin(&min_ray_coefficient, ray_coefficient) && dist <= max_dist)
 					res = i;
+				if (early_return)
+					break;
 			}
 			res;
 	});
@@ -71,16 +73,32 @@ int	closest_object(t_object **objects, t_ray ray)
 	return (index);
 }
 
-t_color	handle_lights(t_object *object, t_vector cross_point, t_light *lights, t_color ambient)
+/*
+ * obj_index: 交差している物体
+ * cross_point: 視線が物体と最も近く交差している位置ベクトル
+ * ray: 交差点から光源への半直線
+ * ray.start = 交差点に微小値を足した位置ベクトル
+ * ray.direction = 交差点から高原へのベクトル
+ * dist = 交差点と光源の距離
+ */
+t_color	handle_lights(t_program *p, int obj_index, t_vector cross_point)
 {
 	t_color	c;
 	int		i;
+	bool	is_covered;
+	t_ray	ray;
+	double	dist;
 
-	c = ambient;
+	c = p->ambient;
 	i = 0;
 	while (i < LIGHT_NUM)
 	{
-		c = color_add(c, object_calc_radiance(object, cross_point, lights[i]));
+		ray.direction = vec_sub(p->lights[i].coordinate, cross_point);
+		ray.start = vec_add(cross_point, vec_mult(ray.direction, EPSILON));
+		dist = vec_magnitude(ray.direction) - EPSILON;
+		is_covered = closest_object(p->objects, ray, dist, true) >= 0;
+		if (!is_covered)
+			c = color_add(c, object_calc_radiance(p->objects[obj_index], cross_point, p->lights[i]));
 		i++;
 	}
 	return (c);
@@ -97,11 +115,11 @@ void	draw(t_program *program, int x, int y)
 	screen_point = init_screen_point(x, y);
 	ray.start = program->camera_point;
 	ray.direction = vec_sub(screen_point, program->camera_point);
-	obj_index = closest_object(program->objects, ray);
+	obj_index = closest_object(program->objects, ray, INFINITY, false);
 	if (obj_index >= 0)
 	{
 		cross_point = vec_add(ray.start, vec_mult(ray.direction, object_solve_ray_equation(program->objects[obj_index], ray)));
-		color = handle_lights(program->objects[obj_index], cross_point, program->lights, program->ambient);
+		color = handle_lights(program, obj_index, cross_point);
 		color = color_map(color);
 		add_color_to_image(&program->img, color_to_int(color), x, y);
 	}
