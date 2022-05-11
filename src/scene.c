b2627cd3c67c6cde4d45d2acfa7bdade6cc151d2
moromin <34294957+moromin@users.noreply.github.com>
2022-05-11 16:49:22 +0900

    refactor: simplify program -> p

--- src/scene.c
+++ src/scene.c
@@ -3,7 +3,7 @@
 // TODO: check duplicate capital letters
 // static bool	check_duplicate_capital_letter()
 
-static char	*load_element(char *line, t_program *program)
+static char	*load_element(char *line, t_program *p)
 {
 	size_t	num;
 	char	**info;
@@ -12,16 +12,19 @@ static char	*load_element(char *line, t_program *program)
 	err = NO_ERR;
 	info = x_split(line, ' ');
 	num = count_2d_array((void **)info);
-	// TODO: handle each element
 	if (num == 3 && !ft_strcmp(info[0], "A"))
-		err = load_ambient(program, &info[1]);
+		err = load_ambient(p, &info[1]);
 	else if (num == 4 && !ft_strcmp(info[0], "C"))
-		err = load_camera(program, &info[1]);
+		err = load_camera(p, &info[1]);
+	else if (num == 4 && !ft_strcmp(info[0], "L"))
+		err = load_light(p, &info[1]);
+	else
+		err = ERR_UNDEFINED_IDENTIFIER;
 	free_2d_array((void ***)&info);
 	return (err);
 }
 
-static void	read_rt_file(char *filename, t_program *program)
+static void	read_rt_file(char *filename, t_program *p)
 {
 	int		fd;
 	char	*line;
@@ -35,7 +38,7 @@ static void	read_rt_file(char *filename, t_program *program)
 		status = x_get_next_line(fd, &line);
 		if (status == GNL_STATUS_DONE)
 			break ;
-		err = load_element(line, program);
+		err = load_element(line, p);
 		if (err != NO_ERR)
 			break ;
 	}
@@ -57,11 +60,11 @@ static bool	check_filename(char *filename)
 	return (true);
 }
 
-void	load_rt_file(int argc, char **argv, t_program *program)
+void	load_rt_file(int argc, char **argv, t_program *p)
 {
 	if (argc != 2)
 		exit_with_error_message(ERR_INVALID_ARGS);
 	if (!check_filename(argv[1]))
 		exit_with_error_message(ERR_INVALID_FILE);
-	read_rt_file(argv[1], program);
+	read_rt_file(argv[1], p);
 }
