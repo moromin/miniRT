7800cce8bad182ebc6e095ff37a670177e26ceab
moromin <moromin933@gmai.com>
2022-05-14 15:08:43 +0900

    fix: error message

--- include/scene.h
+++ include/scene.h
@@ -33,8 +33,8 @@
 # define ERR_MISCONFIGURED_SPHERE "Sphere is misconfigured"
 # define ERR_MISCONFIGURED_PLANE "Plane is misconfigured"
 # define ERR_MISCONFIGURED_CYLINDER "Cylinder is misconfigured"
-# define ERR_DUPLICATE_CAPITAL_IDENTIFIER "Duplicate capitalized identifiers"
-# define ERR_LACK_CAPITAL_IDENTIFIER "Lacks capitalized identifiers"
+# define ERR_DUPLICATE_CAPITAL_IDENTIFIER "Capitalized identifiers are duplicated"
+# define ERR_LACK_CAPITAL_IDENTIFIER "Capitalized identifiers are lack"
 # define ERR_UNDEFINED_IDENTIFIER "Undefined identifier exists"
 
 // Warning messages
