2918cf3078750c256b239bab957b941ac4541bcc
moromin <moromin933@gmai.com>
2022-05-19 18:37:05 +0900

    refactor: i,j -> x,y for get_color_from_image

--- src/bumpmap.c
+++ src/bumpmap.c
@@ -17,10 +17,10 @@ static t_vector		convert_color_to_vector(t_color c)
 t_vector	get_vector_from_normal_map(double u, double v, const t_obj_info *info)
 {
 	const t_vector	tangent = ({
-			const double	j = fmod(u * info->bm_freq_u, 1.0) * info->bm_image.width;
-			const double	i = fmod(v * info->bm_freq_v, 1.0) * info->bm_image.height;
+			const double	x = fmod(u * info->bm_freq_u, 1.0) * info->bm_image.width;
+			const double	y = fmod(v * info->bm_freq_v, 1.0) * info->bm_image.height;
 
-			t_color	c = get_color_from_image(&info->bm_image, (int)j, (int)i);
+			t_color	c = get_color_from_image(&info->bm_image, (int)x, (int)y);
 			c = color_add(color_mult(c, 2), color(-1, -1, -1));
 			convert_color_to_vector(c);
 	});
