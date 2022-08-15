a1afd0ffb976fe10eab0e2220cf7d63ab546f5a1
Takumi Hara <takumihara1226@gmail.com>
2022-08-15 15:32:15 +0900

    style: norm object_radiance.c

--- src/object_radiance.c
+++ src/object_radiance.c
@@ -3,8 +3,16 @@
 #include "../include/vector.h"
 #include "../include/light.h"
 #include "../include/object.h"
-static t_color	calc_radiance_diffuse(t_object *obj, t_vector cross_point, t_light light, t_vector camera2cross);
-static t_color	calc_radiance_specular(t_object *obj, t_vector cross_point, t_light light, t_vector camera2cross);
+static t_color	calc_radiance_diffuse(
+					t_object *obj,
+					t_vector cross_point,
+					t_light light,
+					t_vector camera2cross);
+static t_color	calc_radiance_specular(
+					t_object *obj,
+					t_vector cross_point,
+					t_light light,
+					t_vector camera2cross);
 
 /*
  * radiance_ambient: 環境光の反射光の放射輝度
@@ -14,15 +22,25 @@ static t_color	calc_radiance_specular(t_object *obj, t_vector cross_point, t_lig
  */
 // normal: 正規化された物体面の法線ベクトル
 // incident_direction: 正規化された入射方向ベクトル
-t_color	calc_radiance(t_object *const me, t_vector cross_point, t_light light, t_vector camera2cross)
+t_color	calc_radiance(
+			t_object *const me,
+			t_vector cross_point,
+			t_light light,
+			t_vector camera2cross)
 {
-	const t_color	rd = calc_radiance_diffuse(me, cross_point, light, camera2cross);
-	const t_color	rs = calc_radiance_specular(me, cross_point, light, camera2cross);
+	const t_color	rd =
+			calc_radiance_diffuse(me, cross_point, light, camera2cross);
+	const t_color	rs =
+			calc_radiance_specular(me, cross_point, light, camera2cross);
 
 	return (color_add(rd, rs));
 }
 
-static t_color	calc_radiance_diffuse(t_object *obj, t_vector cross_point, t_light light, t_vector camera2cross)
+static t_color	calc_radiance_diffuse(
+					t_object *obj,
+					t_vector cross_point,
+					t_light light,
+					t_vector camera2cross)
 {
 	t_vector		vec;
 	const t_vector	normal = ({
@@ -42,13 +60,18 @@ static t_color	calc_radiance_diffuse(t_object *obj, t_vector cross_point, t_ligh
 	const t_color	col = object_calc_color(obj, cross_point);
 
 	if (vec_dot(normal, incident_direction) >= 0)
-		return (color_mult(color_prod(col, light.intensity), vec_dot(normal, incident_direction)));
+		return (color_mult(color_prod(col, light.intensity), \
+						   vec_dot(normal, incident_direction)));
 	else
 		return (color(0, 0, 0));
 }
 
 // specular: 入射光の正反射ベクトル
-static t_color	calc_radiance_specular(t_object *obj, t_vector cross_point, t_light light, t_vector camera2cross)
+static t_color	calc_radiance_specular(
+		t_object *obj,
+		t_vector cross_point,
+		t_light light,
+		t_vector camera2cross)
 {
 	t_vector		vec;
 	const t_vector	normal = ({
@@ -61,16 +84,18 @@ static t_color	calc_radiance_specular(t_object *obj, t_vector cross_point, t_lig
 			n = vec_mult(n, -1);
 		n;
 	});
-	const t_vector	incident_direction = vec_normalize(vec_sub(light.pos, cross_point));
+	const t_vector	incident_direction =
+			vec_normalize(vec_sub(light.pos, cross_point));
 	const t_vector	specular = ({
 		vec = vec_mult(normal, 2 * vec_dot(normal, incident_direction));
 		vec_sub(vec, incident_direction);
 	});
-	const t_vector	cross_point_inverse_normalized = vec_normalize(vec_mult(camera2cross, -1));
+	const t_vector	cross_point_inverse_normalized =
+			vec_normalize(vec_mult(camera2cross, -1));
 
 	if (vec_dot(normal, incident_direction) >= 0 && vec_dot(vec, specular) >= 0)
-		return (color_mult(color_prod(obj->material.k_specular, light.intensity),
-				   pow(vec_dot(cross_point_inverse_normalized, specular), SHININESS)));
-	else
-		return (color(0, 0, 0));
+		return (color_mult(\
+			color_prod(obj->material.k_specular, light.intensity), \
+			pow(vec_dot(cross_point_inverse_normalized, specular), SHININESS)));
+	return (color(0, 0, 0));
 }
